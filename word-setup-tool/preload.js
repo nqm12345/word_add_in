fcdfe122fadefc5b31f8fa36e4a68c72958e1459
Minhnqph40837 <minhdaonho@gmail.com>
2025-11-25 13:13:06 +0700

    Remove Electron app from Git, keep WPF version only for clients

--- word-setup-tool/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('setupAPI', {
-  onUpdateStatus: (callback) => ipcRenderer.on('update-status', (event, data) => callback(data)),
-  onSetupComplete: (callback) => ipcRenderer.on('setup-complete', (event, data) => callback(data))
-});
